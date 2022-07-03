67e5af37bdddbfff156c1fdcc0ea27ae5ca3bb3f
Mariia <mariia.kushnir.personal@gmail.com>
2022-07-03 23:53:32 +0300

    Created project

---
+++ script.js
@@ -0,0 +1,7 @@
+let popup = prompt("What is your name?");
+popup = popup.toLowerCase();
+if (popup === "mariia" || popup === "maria" || popup === "mariya") {
+  alert("Nice to meet you");
+} else {
+  alert("Awesome name");
+}
